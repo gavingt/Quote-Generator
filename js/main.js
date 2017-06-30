d264aef982ec44f9de18a4ca13aa14d3fc03e74d
Gavin Wright <gavingt@gmail.com>
2017-06-30 01:37:58 -0700

    switched to HTTPS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(json) {
+$.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(json) {
     $(".quote").html(JSON.stringify(json));
 });
 
